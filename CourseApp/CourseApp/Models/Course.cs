dc0b159771abefa4c1907b910faea79513abf783
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-08-27 15:39:29 +0000

    adding models for course and student

---
+++ CourseApp/CourseApp/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Models
+{
+    public class Course
+    { 
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string TemplateID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ CourseApp/CourseApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Models
+{
+    public class Student
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+    }
+}
