12823250af7415d8d4689636bb7d0e22ea3024e9
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-08-27 15:50:58 +0000

    added Required to models

--- CourseApp/CourseApp/Models/Course.cs
+++ CourseApp/CourseApp/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,19 @@ namespace CourseApp.Models
 {
     public class Course
     { 
+        [Required]
         public int ID { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string TemplateID { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime EndDate { get; set; }
     }
 }
